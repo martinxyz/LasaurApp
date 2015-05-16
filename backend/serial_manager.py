811c12682cdd6b549fc4c90b9e3ee9763d94c6dc
Martin Renold <martinxyz@gmx.ch>
2015-05-16 13:13:54 +0200

    faster status polling
    Change polling timer from 4000ms to 300ms.
    Limit firmware polling to 100ms.
    
    The previous 4000 ms update rate (plus 4000ms lag) got people
    complaining.  The server seems fine with a higher rate, the
    client (browser cpu load) seems to be the bottleneck.

--- backend/serial_manager.py
+++ backend/serial_manager.py
@@ -28,6 +28,7 @@ class SerialManagerClass:
 
         # status flags
         self.status = {}
+        self.last_poll = 0.0
         self.reset_status()
 
         self.LASAURGRBL_FIRST_STRING = "LasaurGrbl"
@@ -149,10 +150,11 @@ class SerialManagerClass:
         return bool(self.device)
 
     def get_hardware_status(self):
-        if self.is_queue_empty():
+        if self.is_queue_empty() and time.time() - self.last_poll > 0.1:
             # trigger a status report
             # will update for the next status request
             self.queue_gcode('?')
+            self.last_poll = time.time()
         return self.status
 
 
