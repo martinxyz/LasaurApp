729e480955f2fbb45ebfbc9d47a7b73d5457db4f
Martin Renold <martin@log2.ch>
2015-12-16 20:31:44 +0100

    raster.py: bugfix and improvements
    Use air support and do homing.
    Bugfix for PIL to numpy array conversion.
    Option to generate a preview image.
    Options for x and y position offset.

--- backend/raster.py
+++ backend/raster.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
-import sys, os, time, random, math
+import sys, os, time
 import numpy as np
 from PIL import Image, ImageOps
 import argparse
 from config import conf
 import driveboard
 
+def PIL2array(img):
+    return np.array(img.getdata(), np.uint8).reshape(img.size[1], img.size[0])
+
 def raster_execute(b, pulse_duration_image, x0, y0, ppmm_x, ppmm_y, skip_empty=True, bidirectional=False, lead_in=5.0):
     """Execute the rastering of a pulse-duration image."""
     assert pulse_duration_image.dtype == 'uint8'
     assert pulse_duration_image.max() < 128, 'image contains pulse durations that are not feasible'
+    #from scipy.misc import imsave
+    #imsave('raster-execute.png', pulse_duration_image)
 
     b.intensity(0)
     direction = +1
@@ -68,47 +73,51 @@ def prepare_and_engrave(b, img, x0, y0, settings):
     error = np.abs(ppmm - s.ppmm)
     i = np.argmin(error)
     pulse = pulse[i]
-    ppmm  = ppmm[i]
+    ppmm = ppmm[i]
     print 'desired ppmm %.3f, actual ppmm %.3f, pulse %d (%.0f us)' % (s.ppmm, ppmm, pulse, pulse * b.PULSE_SECONDS / 1e-6)
     
     feedrate = s.max_feedrate
 
     # limit feedrate such that max_intensity is respected
     feedrate_max = s.max_intensity/100.0 / (ppmm * (pulse * b.PULSE_SECONDS)) * 60 
-    print 'feedrate_max (because of max_intensity)', feedrate_max
+    print 'feedrate limit because of max_intensity: %.0f mm/min' % feedrate_max
     feedrate = min(feedrate, feedrate_max)
 
     # limit feedrate such that the driveboard will not slow down to wait for serial data
     byte_per_second = conf['baudrate']/10/2 # (8 bits + startbit + stopbit) and every byte is sent twice (error detection)
     byte_per_second *= 0.5 # leave some room for parameters and line commands
     feedrate_max = byte_per_second / ppmm * 60.0
-    print 'feedrate_max (because of baudrate limit)', feedrate_max
+    print 'feedrate limit because of baudrate limit: %.0f mm/min' % feedrate_max
     feedrate = min(feedrate, feedrate_max)
 
-    print 'feedrate', feedrate
-    print 'black intensity/duty: %.1f%%' % (ppmm * (pulse * b.PULSE_SECONDS) * (feedrate / 60) * 100)
+    print 'feedrate %.0f' % feedrate
+    print 'intensity (or duty) for full black: %.1f%%' % (ppmm * (pulse * b.PULSE_SECONDS) * (feedrate / 60) * 100)
 
-
-    # scale image to output ppmm
+    # prepare image
+    if img.mode == 'RGBA':
+        white_background = Image.new('RGBA', img.size, color=(255, 255, 255, 255))
+        img = Image.alpha_composite(white_background, img)
     img = img.convert('L') # grayscale
     img = ImageOps.invert(img) # black means high power
+
+    # scale image to output ppmm
     input_w, input_h = img.size
     w = ppmm * s.width
     h = w / float(input_w) * input_h
     w, h = int(round(w)), int(round(h))
     if s.binary:
         img = img.resize((w, h), Image.NEAREST)
-        #img.save('debug1.png')
         img = img.convert('1', dither=Image.NONE)
     else:
         img = img.resize((w, h), Image.ANTIALIAS)
-        #img.save('debug1.png')
         img = img.convert('1', dither=Image.FLOYDSTEINBERG)
-    #img.save('debug2.png')
+
+    if s.save_preview:
+        preview = ImageOps.invert(img.convert('RGB'))
+        preview.save(s.save_preview)
     
-    img = np.array(img, dtype='uint8')
+    img = PIL2array(img)
     img[img>0] = pulse
-    #print img, img.mean(), img.max()
 
     accel_dist = 0.5 * feedrate**2 / b.ACCELERATION
     lead_in_max = accel_dist * 1.2
@@ -121,11 +130,11 @@ def prepare_and_engrave(b, img, x0, y0, settings):
     return w/ppmm, h/ppmm
 
 
-def cut_box(b, x, y, w, h, cut_feedrate, cut_intensity):
+def cut_box(b, x, y, w, h, feedrate, intensity):
     b.intensity(0)
     b.move(x, y)
-    b.feedrate(cut_feedrate)
-    b.intensity(cut_intensity)
+    b.feedrate(feedrate)
+    b.intensity(intensity)
     b.move(x+w, y)
     b.move(x+w, y+h)
     b.move(x, y+h)
@@ -133,19 +142,23 @@ def cut_box(b, x, y, w, h, cut_feedrate, cut_intensity):
     b.intensity(0)
 
 def job(b, args):
+    b.homing()
+    b.air_on()
     if args.cut:
         box_spacing = 5.0
     else:
         box_spacing = 0.0
 
-    x0 = max(box_spacing, args.lead_in)
-    y0 = box_spacing
+    x0, y0 = args.pos_x, args.pos_y
+    x0 += max(box_spacing, args.lead_in)
+    y0 += box_spacing
 
     w, h = prepare_and_engrave(b, Image.open(args.filename[0]), x0, y0, settings=args)
     if args.cut:
         cut_box(b, x0 - box_spacing, y0 - box_spacing, w + 2*box_spacing, h + 2*box_spacing,
                 args.cut_feedrate, args.cut_intensity)
     b.move(0, 0)
+    b.air_off()
 
 def wait():
     print 'waiting for job to execute...'
@@ -166,13 +179,13 @@ if __name__ == '__main__':
 
     p = argparse.ArgumentParser(description="Raster-engrave an image.")
     p.add_argument('filename', nargs=1, metavar='FILENAME',
-                   help='PNG, JPEG or SVG file to be raster-engraved')
+                   help='PNG or JPEG file to be raster-engraved')
 
     p.add_argument('-w', '--width', type=float, required=True,
                    help='width (mm) of the image (height is calculated)')
 
     p.add_argument('-e', '--energy-density', required=True, type=float,
-                   help='exposure of fully black areas (Ws per mm^2), about 0.4 to clearly mark wood, 0.9 to engrave wood')
+                   help='exposure of fully black areas (Ws per mm^2), about 0.3 to mark wood, 0.9 to engrave wood')
 
     p.add_argument('-f', '--max-feedrate', default=8000, type=float,
                    help='maximum feedrate (mm/min)')
@@ -192,30 +205,33 @@ if __name__ == '__main__':
     p.add_argument('--skip-empty', action='store_true',
                    help='skip white pixels (faster, but maybe it can affect quality)')
 
-    p.add_argument('--lead-in', default=20.0, type=float,
-                   help='distance (mm) for acceleration before and after raster lines (default: 20mm; it can be very low or zero; will be limited to 120%% of the calculated acceleration distance)')
+    p.add_argument('--lead-in', default=2.0, type=float,
+                   help='distance (mm) for acceleration before and after raster lines (default: 2mm; acceleration to 8000mm/min requires 22mm, but rastering during acceleration seems to work well)')
 
-    p.add_argument('-c', '--cut', action='store_true',
+    p.add_argument('--pos-x', default=0.0, type=float, help='start x position')
+    p.add_argument('--pos-y', default=0.0, type=float, help='start y position')
+
+    p.add_argument('--cut', action='store_true',
                    help='cut a box around the result')
-    p.add_argument('--cut-feedrate', default=1300, type=float,
-                   help='feedrate for cutting the box, default: 1300')
+    p.add_argument('--cut-feedrate', default=1200, type=float,
+                   help='feedrate for cutting the box')
     p.add_argument('--cut-intensity', default=60, type=float,
-                   help='intensity (in percent) for cutting the box, default: 60')
+                   help='intensity (in percent) for cutting the box')
 
-    p.add_argument('-s', '--simulation', action='store_true',
-                   help='do not connect to laser, use simulation and print statistics')
+    p.add_argument('-s', '--simulate', action='store_true',
+                   help='do not connect to driveboard, print statistics instead')
+    p.add_argument('--simulate-svg', metavar='filename.svg',
+                   help='create SVG output during simulation')
+    p.add_argument('--save-preview', metavar='filename.png',
+                   help='save laser-scaled and dithered image for preview')
+
     args = p.parse_args()
-    print args
-
-    #"inkscape --without-gui --file=filename.svg --export-area-drawing --export-background=white --export-dpi=88.9 --export-png 'foo.png'"
-
     print vars(args)
+    print
 
-
-    if args.simulation:
-        print 'simulation...'
+    if args.simulate:
         import simulation
-        sim = simulation.Simulator()
+        sim = simulation.Simulator(args.simulate_svg)
         job(sim, args)
         sim.report()
         print 'simulation done.'
--- backend/simulation.py
+++ backend/simulation.py
@@ -1,9 +1,8 @@
 import driveboard
-import cairo
 import math
 
 class Simulator:
-    def __init__(self):
+    def __init__(self, svg_output_filename=None):
         self.x = 0.0
         self.y = 0.0
         self.f = 8000.0
@@ -14,42 +13,33 @@ class Simulator:
         self.MAXIMUM_PULSE_TICKS = driveboard.MAXIMUM_PULSE_TICKS
         self.PULSE_SECONDS = driveboard.PULSE_SECONDS
         self.ACCELERATION = driveboard.ACCELERATION
-        self.filename = 'simulated.svg'
-        
-        w, h = 1220, 610
-        mm_to_points = 72 / 25.4
-        surf = cairo.SVGSurface(self.filename, w*mm_to_points, h*mm_to_points)
-        self.cr = cairo.Context(surf)
-        self.cr.scale(mm_to_points, mm_to_points)
+
+        if svg_output_filename:
+            self.filename = svg_output_filename
+            import cairo
+            w, h = 1220, 610
+            mm_to_points = 72 / 25.4
+            surf = cairo.SVGSurface(self.filename, w*mm_to_points, h*mm_to_points)
+            self.cr = cairo.Context(surf)
+            self.cr.scale(mm_to_points, mm_to_points)
+        else:
+            self.cr = None
 
     def homing(self):
         self.x = 0.0
         self.y = 0.0
+        
+    def air_on(self):
+        pass
+
+    def air_off(self):
+        pass
+
     def feedrate(self, f):
         self.f = f
+
     def intensity(self, v):
         self.i = v
-    
-    def _move(self, x, y, raster_data=None):
-        dist = math.hypot(self.x-x, self.y-y)
-        self.dist += dist
-        self.minutes += dist / self.f
-
-        cr = self.cr
-        cr.set_line_width(0.1)
-        if raster_data is not None: # raster move
-            cr.set_source_rgba(0.0, 0.0, 0.0, 0.5)
-        elif self.i == 0: # travel move
-            cr.set_line_width(0.05)
-            cr.set_source_rgba(0.0, 0.0, 1.0, 0.5)
-        else: # lasing move
-            cr.set_source_rgba(1.0, 0.0, 0.0, 0.5)
-        cr.move_to(self.x, self.y)
-        cr.line_to(x, y)
-        cr.close_path()
-        cr.stroke()
-        self.x = x
-        self.y = y
 
     def move(self, x, y):
         self._move(x, y)
@@ -57,8 +47,34 @@ class Simulator:
     def raster_move(self, x, y, data):
         self._move(x, y, raster_data=data)
 
+    def _move(self, x, y, raster_data=None):
+        dist = math.hypot(self.x-x, self.y-y)
+        self.dist += dist
+        self.minutes += dist / self.f
+
+        cr = self.cr
+        if cr:
+            cr.set_line_width(0.1)
+            if raster_data is not None: # raster move
+                cr.set_source_rgba(0.0, 0.0, 0.0, 0.5)
+            elif self.i == 0: # travel move
+                cr.set_line_width(0.05)
+                cr.set_source_rgba(0.0, 0.0, 1.0, 0.5)
+            else: # lasing move
+                cr.set_source_rgba(1.0, 0.0, 0.0, 0.5)
+            cr.move_to(self.x, self.y)
+            cr.line_to(x, y)
+            cr.close_path()
+            cr.stroke()
+
+        self.x = x
+        self.y = y
+
     def report(self):
-        print 'duration: %.1f minutes (without accelerations)' % self.minutes
-        print 'distance: %.1f m' % (self.dist / 1000.0)
-        print 'output: %r' % self.filename
+        print
+        print 'Simulation results:'
+        print 'distance %.0fm' % (self.dist / 1000.0)
+        print 'duration %.1f minutes (without accelerations)' % self.minutes
+        if self.cr:
+            print 'output saved to: %r' % self.filename
 
